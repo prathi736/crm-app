aca02b67b6c92880c5367cd72a9856b74fd1dc96
Pulkit Rathi <63576303+prathi736@users.noreply.github.com>
2023-12-07 15:59:50 +0530

    first commit

---
+++ dcrm/website/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+def home(request):
+    # Check to see if logging in 
+    if request.method == 'POST':
+        username = request.POST[ 'username' ]
+        password = request.POST[ 'password' ]
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "YOU HAVE SUCCESSFULLY LOGGED IN!!")
+            return redirect('home')
+        else:
+            messages.error(request, "THERE IS AN ERROR WHILE LOGGING IN, PLEASE TRY AGAIN!")
+            return redirect('home')
+    else:
+        return render(request, "home.html", {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
