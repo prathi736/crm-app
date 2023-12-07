f595805ccedb06992bfd3d8ab54b951d5b5263e0
Pulkit Rathi <63576303+prathi736@users.noreply.github.com>
2023-12-07 20:44:39 +0530

    added logout

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -20,8 +20,7 @@ def home(request):
     else:
         return render(request, "home.html", {})
 
-def login_user(request):
-    pass
-
 def logout_user(request):
-    pass
+    logout(request)
+    messages.info(request, "YOU HAVE BEEN LOGGED OUT!!")
+    return redirect('home')
