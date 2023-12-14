f603a5c0f868708483d9560dc2d5888238a81ad6
Pulkit Rathi <63576303+prathi736@users.noreply.github.com>
2023-12-14 11:18:49 +0530

    added record modification

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 def home(request):
@@ -54,4 +54,27 @@ def customer_record(request, pk):
         return render(request, 'record.html', {'customer_record':customer_record})
     else:
         messages.error(request, "YOU MUST BE LOGGED IN TO VIEW THIS PAGE!!")
+        return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.error(request, "RECORD HAS BEEN DELETED SUCCESSFULLY!")
+        return redirect('home')
+    else:
+        messages.error(request, "YOU MUST BE LOGGED IN TO DO THAT OPERATION!!")
+        return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "RECORD ADDED SUCCESSFULLY!!")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.info(request, "YOU MUST BE LOGGED IN !!")
         return redirect('home')
