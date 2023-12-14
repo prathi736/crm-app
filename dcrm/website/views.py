b54d075cda6a075896256e2b92ce24c148222fe4
Pulkit Rathi <63576303+prathi736@users.noreply.github.com>
2023-12-14 12:38:27 +0530

    added update record

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -78,3 +78,16 @@ def add_record(request):
     else:
         messages.info(request, "YOU MUST BE LOGGED IN !!")
         return redirect('home')
+    
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "RECORD HAS BEEN UPDATED SUCCESSFULLY!!")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.info(request, "YOU MUST BE LOGGED IN !!")
+        return redirect('home')
